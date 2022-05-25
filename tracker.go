87e17dd59b6dbe493d17eecac3e8a065c0827bbc
Leonardo Campello <leonardocampello.dev@gmail.com>
2022-05-25 08:34:57 -0300

    modularizing the code

--- tracker.go
+++ tracker.go
@@ -1,8 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+	userGreetingMessage()
+
+	displayInitialMenu()
+
+	chosenOption := handleChosenOption()
+
+	switch chosenOption {
+	case 0:
+		os.Exit(0)
+	case 1:
+		fmt.Println("Monitoring")
+	case 2:
+		fmt.Println("Logs")
+	default:
+		fmt.Println("Invalid option")
+
+		os.Exit(-1)
+	}
+}
+
+func userGreetingMessage() {
 	userName := "Leonardo"
 	appVersion := 0.1
 
@@ -10,23 +34,18 @@ func main() {
 	fmt.Println("Current version of the program", appVersion)
 
 	fmt.Println("==============================")
+}
 
-	fmt.Println("(1) - start monitoring")
-	fmt.Println("(2) - display the logs")
-	fmt.Println("(0) - exit program")
-
+func handleChosenOption() int {
 	var chosenOption int
 
 	fmt.Scan(&chosenOption)
 
-	switch chosenOption {
-	case 0:
-		fmt.Println("Leaving")
-	case 1:
-		fmt.Println("Monitoring")
-	case 2:
-		fmt.Println("Logs")
-	default:
-		fmt.Println("Invalid option")
-	}
+	return chosenOption
+}
+
+func displayInitialMenu() {
+	fmt.Println("(1) - start monitoring")
+	fmt.Println("(2) - display the logs")
+	fmt.Println("(0) - exit program")
 }
